c0eed90030dda98feedbf3b0ea3f8ec2bb462b54
takahirom <takam.dev@gmail.com>
2019-09-13 14:42:25 +0900

    Use updated input value

--- src/main/kotlin/UrlExtractor.kt
+++ src/main/kotlin/UrlExtractor.kt
@@ -1,10 +1,10 @@
+import org.w3c.dom.HTMLTextAreaElement
 import org.w3c.dom.get
 import kotlin.browser.document
 
 fun main() {
-    val input = document.forms[0]?.firstElementChild
-    input
-        ?.addEventListener("change", { event ->
+    getInput()
+        .addEventListener("change", { event ->
             apply()
         })
     document.forms[0]?.getElementsByTagName("input")?.get(0)
@@ -16,9 +16,8 @@ fun main() {
 
 fun apply() {
     document.getElementById("extracted")?.innerHTML = buildString {
-        val extracted = document.forms[0]
-            ?.textContent?.split("?")
-            ?.map { it.split("&") } ?: return
+        val extracted = getInput().value.split("?")
+            .map { it.split("&") } ?: return
         append("■ hostname" + "\n")
         append(extracted[0][0] + "\n")
         append("----------\n")
@@ -26,3 +25,5 @@ fun apply() {
         append(extracted[1].joinToString("\n"))
     }
 }
+
+private fun getInput() = document.forms[0]?.firstElementChild as HTMLTextAreaElement
