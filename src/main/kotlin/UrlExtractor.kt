8e32b79832d13404fa8b55d4cb7eaaf0ec66eb5a
takahirom <takam.dev@gmail.com>
2020-02-13 15:11:09 +0900

    Add sort feature

--- src/main/kotlin/UrlExtractor.kt
+++ src/main/kotlin/UrlExtractor.kt
@@ -1,9 +1,14 @@
+import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.HTMLTextAreaElement
 import org.w3c.dom.get
 import kotlin.browser.document
 
 fun main() {
     getInput()
+        .addEventListener("change", { event ->
+            apply()
+        })
+    getCheckBox()
         .addEventListener("change", { event ->
             apply()
         })
@@ -22,8 +27,19 @@ fun apply() {
         append(extracted[0][0] + "\n")
         append("----------\n")
         append("■ parameters" + "\n")
-        append(extracted[1].joinToString("\n"))
+        append(extracted[1]
+            .let {
+                if (getCheckBox().checked) {
+                    it.sortedBy {
+                        it[0]
+                    }
+                } else {
+                    it
+                }
+            }
+            .joinToString("\n"))
     }
 }
 
 private fun getInput() = document.forms[0]?.firstElementChild as HTMLTextAreaElement
+private fun getCheckBox() = document.getElementsByName("sort")[0] as HTMLInputElement
